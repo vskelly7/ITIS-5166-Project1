28ef1d8913586a3c1ee49068585e6bed08c69d03
Me1anch01y <johnksmith8@gmail.com>
2024-09-02 18:59:59 -0400

    Change project topic

--- assets/js/events.js
+++ assets/js/events.js
@@ -1,15 +1,26 @@
 events = {
-  Educational: [],
-  Recreational: [],
-  Travel: [],
+  Educational: [
+    "LCES 2050 - Introduction to Linguistic Anthropology",
+    "ITIS 5166 - Network Based Application Development",
+    "MBAD 6122 - Decison Modeling and Analysis",
+  ],
+  Recreational: [
+    "Fun at Carowinds",
+    "Local Brewery Crawl",
+    "Farmers Market Meet Up",
+  ],
 };
 document.addEventListener("DOMContentLoaded", () => {
   let content = document.querySelector(".content-wrapper");
+  let group = document.createElement("div");
   for (const [key, value] of Object.entries(events)) {
-    content.innerHTML += `<h1>${key}</h1>`;
-    for (const name of events[key]) {
-      content.innerHTML += `<a href="event.html">${name}</a>`;
+    group.innerHTML += `<h1>${key}</h1>`;
+    let list = document.createElement("ul");
+    for (const name of value) {
+      list.innerHTML += `<li><a href="event.html">${name}</a></li>`;
     }
+    group.appendChild(list);
     console.log(key, value);
+    content.appendChild(group);
   }
 });
