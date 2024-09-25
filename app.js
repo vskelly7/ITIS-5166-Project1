62f1bde145feeb479952e7479426c73c0ae95012
Me1anch01y <johnksmith8@gmail.com>
2024-09-25 18:20:46 -0400

    Modify project file

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ let host = "localhost";
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
-app.use(express.static("assets"));
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan("tiny"));
