342d3ce768dfea531cba418fc1dfdaf9b01bed6d
Me1anch01y <johnksmith8@gmail.com>
2024-10-23 19:23:28 -0400

    Fix comments for main routes

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -8,9 +8,10 @@ const router = express.Router();
 //GET /about: displays about page to user
 router.get("/about", mainController.about);
 
-//GET /: displays contact page to user
+//GET /contact: displays contact page to user
 router.get("/contact", mainController.contact);
 
+//GET /: displays the homepage"
 router.get("/", mainController.index);
 
 module.exports = router;
